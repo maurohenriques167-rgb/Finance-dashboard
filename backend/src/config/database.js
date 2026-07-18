1584fa93bda5427fdd8003bd5a41900fb7ae1a5b
Mauro Henrique <mauro.bezerra@escolar.ifrn.edu.br>
2026-07-18 18:57:23 -0300

    Configura banco Aiven

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,13 +1,25 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 const sequelize = new Sequelize(
-    "finance_dashboard",
-    "finance_user",
-    "6N4f879646*",
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-        host: "localhost",
-        dialect: "mysql"
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "mysql",
+
+        dialectOptions:{
+            ssl:{
+                require:true,
+                rejectUnauthorized:false
+            }
+        },
+
+        logging:false
     }
 );
+
 
 module.exports = sequelize;
